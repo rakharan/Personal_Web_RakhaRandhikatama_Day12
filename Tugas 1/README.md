0882e8f82cd5d28e769609321824063ff683ffca
Rakharan <randhikatamar@gmail.com>
2023-02-02 17:53:50 +0700

    Tugas 1 CH-2

---
+++ Tugas 1/README.md
@@ -0,0 +1,2 @@
+# Dumbways-Batch-44-C2
+Chapter 2
--- Tugas 1/tailwind.config.js
+++ Tugas 1/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'cardShadow': 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;'
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp'),],
 }
